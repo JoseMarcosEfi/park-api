dd896281d5dcd3cc4eca2c56856da9a97af79b0a
Jose Marcos <thelegendchase2013@hotmail.com>
2024-05-21 11:06:05 -0300

    primeira requisição usuario

--- demo-park-api/src/main/java/com/jmarcos/demoparkapi/service/UsuarioService.java
+++ demo-park-api/src/main/java/com/jmarcos/demoparkapi/service/UsuarioService.java
@@ -1,11 +1,20 @@
 package com.jmarcos.demoparkapi.service;
 
+import com.jmarcos.demoparkapi.entity.Usuario;
 import com.jmarcos.demoparkapi.repository.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.security.PublicKey;
 
 @Service
 @RequiredArgsConstructor
 public class UsuarioService {
     private final UsuarioRepository usuarioRepository;
+
+    @Transactional
+     public Usuario salvar(Usuario usuario){
+         return usuarioRepository.save(usuario);
+     }
 }
