bd58860b77962231a8bda5474c8a8079f4f1846a
Jose Marcos <thelegendchase2013@hotmail.com>
2024-06-12 15:52:31 -0300

    adição do da documentação do swagger e feito a documentação de alguns itens de usuarios

---
+++ demo-park-api/src/main/java/com/jmarcos/demoparkapi/config/SpringDocOpenApiConfig.java
@@ -0,0 +1,24 @@
+package com.jmarcos.demoparkapi.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringDocOpenApiConfig {
+    @Bean
+    public OpenAPI openApi(){
+        return new OpenAPI()
+                .info(
+                        new Info()
+                                .title("Rest API - Spring Park")
+                                .description("Api para gestão de estacionamento de veículos")
+                                .version("v1")
+                                .license(new License().name("Apache 2.0").url("https://apache.org/licenses/LICENSE-2.0"))
+                                .contact(new Contact().name("Jose Marcos").email("jose.marcos.efi@gmail.com"))
+                );
+    }
+}
