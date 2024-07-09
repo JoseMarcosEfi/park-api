2fae4cb1509e181b357dc8c27758922149fd859c
Jose Marcos <thelegendchase2013@hotmail.com>
2024-07-09 15:55:44 -0300

    Criação JwtUserDetailsService

--- demo-park-api/src/main/java/com/jmarcos/demoparkapi/repository/UsuarioRepository.java
+++ demo-park-api/src/main/java/com/jmarcos/demoparkapi/repository/UsuarioRepository.java
@@ -2,6 +2,13 @@ package com.jmarcos.demoparkapi.repository;
 
 import com.jmarcos.demoparkapi.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Optional;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    Optional<Usuario> findByUsername(String username);
+
+    @Query("SELECT u.role from Usuario u where u.username like :username")
+    Usuario.Role findRoleByUsername(String username);
 }
