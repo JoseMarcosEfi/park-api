1e0401589e7331bb99677778183ba0eb2a262d1d
Jose Marcos <thelegendchase2013@hotmail.com>
2024-07-10 16:45:58 -0300

    bug fix em catch e import

--- demo-park-api/src/main/java/com/jmarcos/demoparkapi/web/controller/AutenticacaoController.java
+++ demo-park-api/src/main/java/com/jmarcos/demoparkapi/web/controller/AutenticacaoController.java
@@ -12,12 +12,12 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.security.sasl.AuthenticationException;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -42,7 +42,7 @@ public class AutenticacaoController {
             return ResponseEntity.ok(token);
         }catch (AuthenticationException ex){
 
-            log.warn("Bad Credentials from username '{}'"), dto.getUsername());
+            log.warn("Bad Credentials from username '{}'", dto.getUsername());
         }
         return ResponseEntity
                 .badRequest()
