0ffde3354926aff541c5a0ce36033f0047a27170
Jose Marcos <thelegendchase2013@hotmail.com>
2024-08-09 16:46:32 -0300

    acesso outros usuários via admin

--- demo-park-api/src/main/java/com/jmarcos/demoparkapi/web/controller/UsuarioController.java
+++ demo-park-api/src/main/java/com/jmarcos/demoparkapi/web/controller/UsuarioController.java
@@ -16,6 +16,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -50,6 +51,7 @@ public class UsuarioController {
                             content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErrorMessage.class)))
             })
     @GetMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<UsuarioResponseDto> getById(@PathVariable Long id){
         Usuario user = usuarioService.buscarPorId(id);
         return ResponseEntity.ok(UsuarioMapper.toDto(user));
